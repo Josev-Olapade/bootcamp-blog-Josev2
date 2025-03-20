370dedc1afa4998a72ffd8a57908cb25c83ddbf7
Joseph Olapade <olapade.j@yahoo.com>
2025-03-20 17:52:11 +0000

    add basic workflow

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,8 @@ module.exports = {
     }]
   },
   testMatch: ['**/*.test.ts'],
-  moduleDirectories: ['node_modules', '<rootDir>']
-  collectCoverage: 'true',
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'text']
 };
